556b98a47034c800d5bed3ab257f3024870f9cc1
Sykotes <sykoted@gmail.com>
2023-11-27 20:41:18 +0000

    day2 part2

--- src/day2.zig
+++ src/day2.zig
@@ -41,3 +41,78 @@ pub fn day2_part1() !void {
     }
     std.debug.print("Score: {}\n", .{score});
 }
+
+pub fn day2_part2() !void {
+    const file = try std.fs.cwd().openFile("input_files/day2.txt", .{ .mode = .read_only });
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const in_stream = buf_reader.reader();
+
+    const lose = 1;
+    const draw = 2;
+    const win = 3;
+
+    const rock: u8 = 1;
+    const paper: u8 = 2;
+    const scissors: u8 = 3;
+
+    var oponent_move: u8 = undefined;
+    var your_move: u8 = undefined;
+    var score: u32 = 0;
+
+    var buffer: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        oponent_move = line[0] - 64;
+        your_move = line[2] - 87;
+
+        switch (your_move) {
+            lose => {
+                switch (oponent_move) {
+                    rock => {
+                        score += scissors;
+                    },
+                    paper => {
+                        score += rock;
+                    },
+                    scissors => {
+                        score += paper;
+                    },
+                    else => {},
+                }
+            },
+            draw => {
+                score += 3;
+                switch (oponent_move) {
+                    rock => {
+                        score += rock;
+                    },
+                    paper => {
+                        score += paper;
+                    },
+                    scissors => {
+                        score += scissors;
+                    },
+                    else => {},
+                }
+            },
+            win => {
+                score += 6;
+                switch (oponent_move) {
+                    rock => {
+                        score += paper;
+                    },
+                    paper => {
+                        score += scissors;
+                    },
+                    scissors => {
+                        score += rock;
+                    },
+                    else => {},
+                }
+            },
+            else => {},
+        }
+    }
+    std.debug.print("Score: {}\n", .{score});
+}
--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,5 @@ const day1 = @import("day1.zig");
 const day2 = @import("day2.zig");
 
 pub fn main() !void {
-    try day2.day2_part1();
+    try day2.day2_part2();
 }
