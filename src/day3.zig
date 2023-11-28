16710c18d266c21217c6cdc5d4c4cc4ed9696622
Sykotes <sykoted@gmail.com>
2023-11-28 19:59:17 +0000

    day3 part1

---
+++ src/day3.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn day3_part1() !void {
+    const file = try std.fs.cwd().openFile("input_files/day3.txt", .{ .mode = .read_only });
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const in_stream = buf_reader.reader();
+
+    var first_half: []u8 = undefined;
+    var second_half: []u8 = undefined;
+    var count: u32 = 0;
+
+    var buffer: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        count += 1;
+        first_half = line[0 .. line.len / 2];
+        second_half = line[line.len / 2 .. line.len];
+
+        outer: for (first_half, 0..) |c1, i| {
+            _ = i;
+            for (second_half, 0..) |c2, j| {
+                _ = j;
+                if (c1 == c2) {
+                    if (c1 >= 'a') {
+                        count += c1 - 'a';
+                    } else {
+                        count += (c1 - 'A') + 26;
+                    }
+                    break :outer;
+                }
+            }
+        }
+    }
+    std.debug.print("Day3 Part1) Total: {}\n", .{count});
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,11 @@
 const day1 = @import("day1.zig");
 const day2 = @import("day2.zig");
+const day3 = @import("day3.zig");
 
 pub fn main() !void {
-    try day1.day1_part1();
-    try day1.day1_part2();
-    try day2.day2_part1();
-    try day2.day2_part2();
+    // try day1.day1_part1();
+    // try day1.day1_part2();
+    // try day2.day2_part1();
+    // try day2.day2_part2();
+    try day3.day3_part1();
 }
